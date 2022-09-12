ca63cd8a97daf72e80e810ddab38656b00dafb9c
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-12 17:58:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-zatvgn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-zatvgn)
---
+++ random.js
@@ -0,0 +1,13 @@
+// function sayHello(){
+//   alert('Hello!')
+// }
+
+// sayHello()
+// sayHello()
+// sayHello()
+
+function sayHi(){
+  return "Hello World!";
+}
+
+alert(sayHi());
